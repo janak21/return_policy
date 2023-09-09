89d937f3c41b1bae3f62bf8bd20fd66b10ed6f4f
unknown <sawalejanak5@gmail.com>
2023-09-09 14:33:53 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
+require('dotenv').config();
+// Connect to MySQL database
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database');
+});
+
+
+// API endpoint to check return eligibility for the last purchased product
+app.get('/checkLastProductReturnEligibility/:CustomerID', (req, res) => {
+  const { CustomerID } = req.params; // CustomerID is the email address
+  const sql = `
+    SELECT o.ProductID, o.PurchaseDate, p.ProductName 
+    FROM Orders o
+    JOIN Products p ON o.ProductID = p.ProductID
+    WHERE o.CustomerID = ?
+    ORDER BY o.PurchaseDate DESC LIMIT 1`;
+  
+  db.query(sql, [CustomerID], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (results.length > 0) {
+      const currentDate = new Date();
+      const purchaseDate = new Date(results[0].PurchaseDate);
+      const returnDeadline = new Date(purchaseDate);
+      returnDeadline.setDate(purchaseDate.getDate() + 90); // 90-day return policy
+      const formattedReturnDeadline = returnDeadline.toISOString().split('T')[0];
+      const isEligible = currentDate <= returnDeadline;
+      res.json({ 
+        isEligible,
+        ProductID: results[0].ProductID,
+        ProductName: results[0].ProductName,
+        PurchaseDate: results[0].PurchaseDate,
+        ReturnDeadline: isEligible ? formattedReturnDeadline : 'Not eligible for return'
+      });
+    } else {
+      res.status(404).json({ message: 'No orders found for this customer' });
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
